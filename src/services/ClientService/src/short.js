4ea0414f3f71764cc37e9499f69ac467c023d050
Rafael Bayer <rafibayer7@gmail.com>
2020-12-05 14:17:28 -0800

    working link shortcuts from client!

--- src/services/ClientService/src/short.js
+++ src/services/ClientService/src/short.js
@@ -1,15 +1,40 @@
+const {GetShortcutRequest} = require('./apiservice_pb.js');
+const {APIServiceClient} = require('./apiservice_grpc_web_pb.js');
+
 function hello() {
     const h1 = document.createElement("h1");
     h1.innerText = "ShortCx! Redirecting..."
     return h1
 }
 
+function backupLink(url) {
+    const p = document.createElement("p");
+    p.innerText = "You should be redirected shortly...";
+    const a = document.createElement("a");
+    a.href = url;
+    a.title = "Click Here to go now!"
+    p.appendChild(a);
+    return p;
+}
+
 function getToken() {
     let segments = window.location.href.split("/");
-    const token = segments.pop() || segments.pop();
+    let token = segments.pop() || segments.pop();
     console.log("Token: " + token);
     return token;
 }
 document.body.appendChild(hello());
-
 let token = getToken();
+
+let shortCxClient = new APIServiceClient("http://localhost:8080");
+let request = new GetShortcutRequest();
+request.setUrlToken(token);
+shortCxClient.getShortcut(request, {}, (err, resp) => {
+    if (err) {
+        console.error(err);
+    } else {
+        let destination = resp.toObject().targetUrl;
+        document.body.appendChild(backupLink(destination));
+        window.location.replace(destination);
+    }
+});
